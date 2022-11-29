45e446ca6078c590f51bede7700a98bddb833c1e
Simone_Campilli <simo.camp71@gmail.com>
2022-11-29 11:40:03 +0100

    Code refactoring done

--- Code/automata-1/runtime/runtime.h
+++ Code/automata-1/runtime/runtime.h
@@ -1,16 +1,15 @@
 #ifndef _RUNTIME_H_
 #define _RUNTIME_H_
 
-#include <string>
-
 /*
  * Gets next inputs, or 'x' if no inputs available.
  * WARNING: this function shifts the input, once you call it!
  */
 char nextChar();
 
-void debug();
-
-void write(std::string);
+/*this function is used to set dub string */
+void debug(char str[]);
+/*this function is used to write output */
+void write(char str[]);
 
 #endif
--- Code/automata-1/runtime/unix/nextCharLib.c
+++ Code/automata-1/runtime/unix/nextCharLib.c
@@ -1,6 +1,5 @@
 #include "runtime.h"
-
-using namespace std;
+#include <stdio.h>
 
 const char inputs[] = {'a', 'a', 'b', 'c'};
 int ninputs = sizeof(inputs) / sizeof(inputs[0]);
@@ -11,4 +10,14 @@ char nextChar()
     if (i >= ninputs)
         return 'x';
     return inputs[i++];
+}
+
+void write(char str[])
+{
+    printf("%s\n", str);
+}
+
+void debug(char str[])
+{
+    printf("[DEBUG] %s\n", str);
 }
